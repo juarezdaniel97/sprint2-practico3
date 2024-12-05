218f6ddeee6c85b59671ac6b906e78a39d4d8c87
Daniel_juarez97 <juarezz.daniel97@gmail.com>
2024-12-05 20:19:28 -0300

    sprint 4 TERMINADO

--- src/app.mjs
+++ src/app.mjs
@@ -1,9 +1,8 @@
 import { connectDB } from "./config/dbConfig.mjs";
 import express from 'express';
 import superHeroRouter from './routes/SuperheroRoutes.mjs'
+import expressEjsLayouts from "express-ejs-layouts";
 
-//agrego 
-//import ejs from 'ejs';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,18 +12,21 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', './view')
 
+//Configuración Express-ejs-layout
+app.use(expressEjsLayouts);
+// app.set('layout', 'layout');
+
 //Conexión a la Base de Datos
 connectDB();
 
 //Para archivos estáticos
 app.use(express.static('public'));
-
 //Configuración de Rutas
 app.use('/api',superHeroRouter);
 
 
 app.use((req, res) =>{
-    res.status(404).render('404');
+    res.status(404).render('404',{ title: '404 - Página no encontradaperHero' });
 });
 
 
--- src/public/js/menu.js
+++ src/public/js/menu.js
@@ -3,15 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const sections = document.querySelectorAll(".section");
 
     menuItems.forEach((item) => {
-        item.addEventListener("click", (event) => {
-            event.preventDefault(); // Previene el comportamiento predeterminado del enlace
-
+        item.addEventListener("click", () => {
             // Ocultar todas las secciones
-            sections.forEach((section) => section.classList.add("d-none"));
+            sections.forEach((section) => section.classList.add("hidden"));
 
             // Mostrar la sección correspondiente
             const sectionId = item.getAttribute("data-section");
-            document.getElementById(sectionId).classList.remove("d-none");
+            document.getElementById(sectionId).classList.remove("hidden");
         });
     });
 });
+
