f9503a09c7958dbd2ba0259c060f9ebc886751b3
Daniel_juarez97 <juarezz.daniel97@gmail.com>
2024-12-06 00:02:12 -0300

    Formato de fecha y hora

--- src/public/js/modalListar.js
+++ src/public/js/modalListar.js
@@ -1,7 +1,7 @@
 
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    
+document.addEventListener('DOMContentLoaded', () => {
+
     // Configuración para actualizar datos del modal dinámicamente
     const modalDetalles = document.getElementById('modalDetalles');
     modalDetalles.addEventListener('show.bs.modal', function (event) {
@@ -16,7 +16,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
         const enemigo = button.getAttribute('data-enemigo');
         const autor = button.getAttribute('data-autor');
         const fecha = button.getAttribute('data-fecha');
-        
+
+        // Convertir la fecha en un objeto Date
+        const formatoFecha = new Date(fecha);
+
+        // Formatear la fecha en "dd/mm/aaaa hh:mm:ss"
+        const dia = formatoFecha.getDate().toString().padStart(2, '0');
+        const mes = (formatoFecha.getMonth() + 1).toString().padStart(2, '0');
+        const anio = formatoFecha.getFullYear();
+        const horas = formatoFecha.getHours().toString().padStart(2, '0');
+        const minutos = formatoFecha.getMinutes().toString().padStart(2, '0');
+        const segundos = formatoFecha.getSeconds().toString().padStart(2, '0');
+
+        // Formatear el resultado
+        const fechaFormateada = `${dia}/${mes}/${anio} ${horas}:${minutos}:${segundos}`;
+
+        console.log(fechaFormateada);
+
+
         // Asignar datos al modal
         modalDetalles.querySelector('#modal-id').textContent = id
         modalDetalles.querySelector('#modal-nombre').textContent = nombre;
@@ -24,6 +41,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
         modalDetalles.querySelector('#modal-aliado').textContent = aliado;
         modalDetalles.querySelector('#modal-enemigo').textContent = enemigo;
         modalDetalles.querySelector('#modal-autor').textContent = autor;
-        modalDetalles.querySelector('#modal-fecha').textContent = fecha;
+        modalDetalles.querySelector('#modal-fecha').textContent = fechaFormateada;
     });
 })
